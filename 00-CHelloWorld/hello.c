75a2bd6047428946abcf86051d0aa58b00f98d16
Ali Mahfoud <amahfoud@frba.utn.edu.ar>
2024-04-17 03:54:45 -0300

    again

--- 00-CHelloWorld/hello.c
+++ 00-CHelloWorld/hello.c
@@ -8,10 +8,13 @@ int main(void) {
   
   // Ejecute el programa con la salida redireccionada a un archivo
   // output.txt; verifique su contenido.
-  // char filename[] = "output.txt";
-  // FILE* outputFile = fopen(filename, "w");
-  // fprintf(outputFile, "%s\n", expectedOutput);
-  // fclose(outputFile);
+  char filename[] = "output.txt";
+  FILE* outputFile = fopen(filename, "w");
+  fprintf(outputFile, "%s\n", expectedOutput);
+  fclose(outputFile);
+
+  // o simplemente usando el operador > para tirar el output de correr el programa dentro de un archvio nuevo
+  // gcc ./hello.c && ./a.out > output.txt
 
   return 0;
 }
